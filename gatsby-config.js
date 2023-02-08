84b4310cb7ef13cd9c881a207cc3be7fbea76190
Taliesin <goldenblossom37@gmail.com>
2023-02-08 16:25:29 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `class demo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
