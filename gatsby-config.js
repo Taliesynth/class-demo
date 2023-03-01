42727bcbc812e19a993d96ffc1c400707016529a
Taliesin <goldenblossom37@gmail.com>
2023-03-01 18:25:17 -0500

    Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/class-demo",
   siteMetadata: {
     title: `Class Demo`,
     siteUrl: `https://www.yourdomain.tld`,
@@ -9,6 +10,17 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "DrupalGraqhQL",
+        // This is field under which it's accessible
+        fieldName: "Drupal",
+        url: "https://csc496f22demo.tldr.dev/graphql",
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
